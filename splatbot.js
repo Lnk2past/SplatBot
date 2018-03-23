91f353ece2122a5c585314dd6ca4944250cd72a5
Lnk2past <Lnk2past@gmail.com>
2018-03-23 00:28:20 -0400

    Add hooks for getting stats on currently equipped weapon/gear.

--- splatbot.js
+++ splatbot.js
@@ -4,7 +4,8 @@ var Discord = require('discord.js');
 
 // Configuration
 var auth = require('./auth.json');
-var splat = require('./splatnet_config.json')
+var config = require('./splatbot_config.json');
+var splat = require('./splatnet_config.json');
 
 // Configure request
 var jar = request.jar();
@@ -18,22 +19,73 @@ var options = {
    jar: jar
 };
 
-// Callback when a "!splat" is triggered.
+// Utility function for reading gear data and putting into a report
+function read_gear(player, gear_type) {
+    gear_report = ''
+    gear = player[gear_type];
+    gear_skills = player[gear_type+'_skills'];
+    gear_report += ' - ' + gear_type + ': ' + gear['name'] + ' by ' + gear['brand']['name'] + '\n';
+    gear_report += '    - Main Skill: ' + gear_skills['main']['name'] + '\n'
+    var counter = 1;
+    for (var sub_idx in gear_skills['subs']) {
+        var sub = gear_skills['subs'][sub_idx];
+        if (sub != null) {
+            gear_report += '    - Sub Skill #' + counter + ': ' + sub['name'] + '\n';
+            counter++;
+        }
+    }
+    gear_report += '\n';
+    return gear_report;
+}
+
+// Callback when a '!splat' is triggered
 function get_splat_data(msg) {
     request(options, function (error, response, body) {
         if (error) {
-            console.log(response.statuscode)
-            console.log(error)
+            console.log(response.statuscode);
+            console.log(error);
         }
         else {
             var json = JSON.parse(body);
-            var report = "";
-            report += 'Player:' + json['records']['player']['nickname'] + ' lv.' + json['records']['player']['player_rank'] + '\n';
+            var player = json['records']['player']
+            var report = '```';
+            report += 'Player:' + player['nickname'] + ' lv.' + player['player_rank'] + '\n';
             report += '--- Current Rankings ---\n'
-            report += "Clam Blitz: " + json['records']['player']['udemae_clam']['name'] + '\n';
-            report += "Tower Control: " + json['records']['player']['udemae_tower']['name'] + '\n';
-            report += "Splat Zones: " + json['records']['player']['udemae_zones']['name'] + '\n';
-            report += "Rainmaker: " + json['records']['player']['udemae_rainmaker']['name'] + '\n';
+            report += ' - Clam Blitz: ' + player['udemae_clam']['name'] + '\n';
+            report += ' - Tower Control: ' + player['udemae_tower']['name'] + '\n';
+            report += ' - Splat Zones: ' + player['udemae_zones']['name'] + '\n';
+            report += ' - Rainmaker: ' + player['udemae_rainmaker']['name'] + '\n';
+            report += '```'
+            msg.channel.send(report);
+        }
+    });
+}
+
+// Callback when a '!splat-gear' is triggered
+function get_splat_gear_data(msg) {
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(response.statuscode);
+            console.log(error);
+        }
+        else {
+            var json = JSON.parse(body);
+            var player = json['records']['player'];
+            var report = '```';
+            report += 'Player:' + player['nickname'] + ' lv.' + player['player_rank'] + '\n';
+            report += '--- Current Gear ---\n'
+
+            weapon = player['weapon']
+            report += ' - Weapon: ' + weapon['name'] + '\n';
+            report += '    - Sub: ' + weapon['sub']['name'] + '\n';
+            report += '    - Special: ' + weapon['special']['name'] + '\n\n';
+
+            report += read_gear(player, 'clothes');
+            report += read_gear(player, 'shoes');
+            report += read_gear(player, 'head');
+
+            report += '```';
+
             msg.channel.send(report);
         }
     });
@@ -50,8 +102,19 @@ bot.on('ready', () => {
 
 // Message hook
 bot.on('message', msg => {
-    if (msg.content === '!splat') {
-       get_splat_data(msg);
+    // Report if the message author is listed or if there are no listed listeners
+
+    author = msg.author.username + '#' + msg.author.discriminator;
+    console.log(author);
+
+    if (config.listen_to.length == 0 || config.listen_to.includes(author)) {
+        if (msg.content == '!splat') {
+            get_splat_data(msg);
+        }
+
+        if (msg.content == '!splat-gear') {
+            get_splat_gear_data(msg);
+        }
     }
 });
 
